f33a335ab3b6a1caa787ea2aebab5f67b6244ed1
Quân Lê Minh <20520708@ms.uit.edu.vn>
2021-10-18 00:02:13 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace WindowApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDuration = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cbAction = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.tbDuration);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(170, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(356, 61);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(304, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "minute(s)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tbDuration
+            // 
+            this.tbDuration.Location = new System.Drawing.Point(110, 16);
+            this.tbDuration.Name = "tbDuration";
+            this.tbDuration.Size = new System.Drawing.Size(188, 20);
+            this.tbDuration.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Duration:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.cbAction);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(170, 102);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(356, 61);
+            this.panel2.TabIndex = 1;
+            // 
+            // cbAction
+            // 
+            this.cbAction.FormattingEnabled = true;
+            this.cbAction.Items.AddRange(new object[] {
+            "ShutDown",
+            "Restart",
+            "LogOut",
+            "Sleep"});
+            this.cbAction.Location = new System.Drawing.Point(110, 16);
+            this.cbAction.Name = "cbAction";
+            this.cbAction.Size = new System.Drawing.Size(188, 21);
+            this.cbAction.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Action:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnCancel);
+            this.panel3.Controls.Add(this.btnOk);
+            this.panel3.Location = new System.Drawing.Point(280, 181);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(113, 20);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 53);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(0, 20);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 53);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(277, 165);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(0, 13);
+            this.lbStatus.TabIndex = 3;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WindowApp";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDuration;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox cbAction;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WindowApp
+{
+   
+    public partial class Form1 : Form
+    {
+        [DllImport("PowrProf.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+        [DllImport("user32")]
+        public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private int TimeLeft;
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            string selected = cbAction.SelectedItem.ToString();
+            int option;
+            if(selected == "ShutDown")
+            {
+                option = 1;
+            }
+            else
+            {
+                if(selected == "Restart")
+                {
+                    option = 2;
+                }
+                else
+                {
+                    if(selected == "LogOut")
+                    {
+                        option = 3;
+                    }
+                    else
+                    {
+                        option = 4;
+                    }
+                }
+            }
+            switch (option)
+            {
+                case 1:
+                    {
+                        timer1.Interval = 1000;
+                        timer1.Enabled = true;
+                        TimeLeft = int.Parse(tbDuration.Text) * 60;
+                        break;
+                    }
+                case 2:
+                    {
+                        timer2.Interval = 1000;
+                        timer2.Enabled = true;
+                        TimeLeft = int.Parse(tbDuration.Text) * 60;
+                        break;
+                    }
+                case 3:
+                    {
+                        timer3.Interval = 1000;
+                        timer3.Enabled = true;
+                        TimeLeft = int.Parse(tbDuration.Text) * 60;
+                        break;
+                    }
+                case 4:
+                    {
+                        timer4.Interval = 1000;
+                        timer4.Enabled = true;
+                        TimeLeft = int.Parse(tbDuration.Text) * 60;
+                        break;
+                    }
+                default:
+                    break;
+            }
+            this.tbDuration.Enabled = false;
+            this.cbAction.Enabled = false;
+            this.btnOk.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(TimeLeft > 0)
+            {
+                lbStatus.Text = "Sẽ tắt máy sau " + TimeLeft + " giây.";
+                TimeLeft = TimeLeft - 1;
+            }
+            else
+            {
+                this.timer1.Stop();
+                this.timer1.Enabled = false;
+                System.Diagnostics.Process.Start("shutdown", "-s -f -t 0");
+                lbStatus.Text = "Đã thực hiện";
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Bạn có muốn dừng chương trình không?", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.timer1.Stop();
+                this.timer1.Enabled = false;
+                lbStatus.Text = "";
+                this.tbDuration.Enabled = true;
+                this.cbAction.Enabled = true;
+                this.btnOk.Enabled = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát chương trình không?", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (TimeLeft > 0)
+            {
+                lbStatus.Text = "Sẽ restart máy tính sau " + TimeLeft + " giây.";
+                TimeLeft = TimeLeft - 1;
+            }
+            else
+            {
+                this.timer2.Stop();
+                this.timer2.Enabled = false;
+                System.Diagnostics.Process.Start("shutdown", "-r -f -t 0");
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if(TimeLeft > 0)
+            {
+                lbStatus.Text = "Sẽ logout ra khỏi tài khoản sau " + TimeLeft + " giây.";
+                TimeLeft = TimeLeft - 1;
+            }
+            else
+            {
+                this.timer3.Stop();
+                this.timer3.Enabled = false;
+                ExitWindowsEx(0, 0);
+                lbStatus.Text = "Đã thực hiện";
+            }
+        }
+       
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (TimeLeft > 0)
+            {
+                lbStatus.Text = "Sẽ vào chế độ sleep sau " + TimeLeft + " giây.";
+                TimeLeft = TimeLeft - 1;
+            }
+            else
+            {
+                this.timer4.Stop();
+                this.timer4.Enabled = false;
+                SetSuspendState(false, true, true);
+                lbStatus.Text = "Đã thực hiện";
+            }
+        }
+    }
+}
